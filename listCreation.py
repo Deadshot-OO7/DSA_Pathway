34773561a043bf6dd0a2f0c022ca1ecf9792a6c2
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2022-02-08 23:53:54 +0530

    Day_16

---
+++ listCreation.py
@@ -0,0 +1,13 @@
+
+list1 = [1,2,3,4,5]
+
+misList=[1,2,"priya",3.4]
+
+nestedList = [1,2,"Akash",4.5,65.8,[32,"Sahu"],[23,34,45]]
+
+emptyList=[]
+
+print(list1)
+print(misList)
+print(nestedList)
+print(emptyList)
