bf91fa6c7902bbdef91315061950c2f6fb94b8e8
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-04-08 22:50:33 +0530

    Java Dsa Day 36 - syntax.java

---
+++ syntax.java
@@ -0,0 +1,15 @@
+package One_D_Arrays;
+
+public class syntax {
+    public static void main(String[] args) {
+
+        int[] a ={23,34,45,56,67};
+
+//        System.out.print(a[3]);
+
+        for (int j : a) {
+            System.out.println(j);
+
+        }
+    }
+}
