f993ebfa46a029d14aa4121d2a1323b5e996b6c6
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-08-22 21:52:09 +0530

    Dsa Revision Day 1

---
+++ Print.java
@@ -0,0 +1,8 @@
+package DSA_Revision;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi");
+    }
+}
---
+++ right_angle_star.java
@@ -0,0 +1,12 @@
+package DSA_Revision;
+
+public class right_angle_star {
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 4 ; i++){
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
