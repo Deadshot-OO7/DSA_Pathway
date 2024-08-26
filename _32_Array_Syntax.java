35e1bcf40d9813513c561d2f79b4c1cddd0e5bd0
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-08-26 23:55:27 +0530

    Dsa Day 4 revision continue

---
+++ _32_Array_Syntax.java
@@ -0,0 +1,16 @@
+package DSA_Revision;
+
+public class _32_Array_Syntax {
+    public static void main(String[] args) {
+
+        int[] marks = new int[3];
+
+        marks[0] = 34;
+        marks[1] = 45;
+        marks[2] = 55;
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
