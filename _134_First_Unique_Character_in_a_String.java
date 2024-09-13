f9091ac20c090b0d9aebfbdc25baa90806a6681e
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-09-13 23:31:04 +0530

    Dsa Day 19 - First_Unique_Character_in_a_String.java

--- _134_First_Unique_Character_in_a_String.java
+++ _134_First_Unique_Character_in_a_String.java
@@ -4,33 +4,25 @@ import java.util.Hashtable;
 import java.util.Map;
 
 public class _134_First_Unique_Character_in_a_String {
-    public static void main(String[] args) {
-        String s = "eeltcode";
-        int count = 0;
-        Hashtable<Character,Integer> ht = new Hashtable<Character,Integer>();
+    public static int main(String[] args) {
+        String s = "leetcode";
+        char[] strTochar = s.toCharArray();
+        //If in testcases, there is a long string which is giving TLE
+        //then you can use array.
 
-        for (int i = 0; i < s.length()-1; i++) {
-            count = 1;
-            for (int j = i+1; j <s.length() ; j++) {
-                if(s.charAt(i)==s.charAt(j))
-                    ht.put(s.charAt(i),++count);
-            }
-            if(count == 1)
-                ht.put(s.charAt(i),count);
-        }
+        for (int i = 0; i < strTochar.length; i++) {
+            for (int j = 0; j < strTochar.length; j++) {
+                if (i != j && strTochar[i] == strTochar[j]) {
+                    break;
+                }
 
-        if(!ht.containsKey(s.charAt(s.length()-1))){
-            ht.put(s.charAt(s.length()-1),1);
-        }
-
-        System.out.println(ht);
-
-        for (Map.Entry<Character,Integer> entry : ht.entrySet()) {
-            if (entry.getValue().equals(1)) {
-                System.out.println(entry.getKey());
-                break;
+                if (j == strTochar.length - 1) {
+                    return i;
+                }
             }
         }
+
+        return -1;
 
     }
 }
