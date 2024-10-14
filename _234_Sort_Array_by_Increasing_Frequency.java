55b14ed1c356af1ae4d1cfce6b1791eeaa8e3695
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-10-14 21:14:56 +0530

    Dsa Day 50 - Sorting

--- _234_Sort_Array_by_Increasing_Frequency.java
+++ _234_Sort_Array_by_Increasing_Frequency.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class _234_Sort_Array_by_Increasing_Frequency {
     public static void main(String[] args) {
         int[] nums = {-1,1,-6,4,5,-6,1,4,1};
+//        Arrays.sort(nums);
+//        for (int n : nums)
+//            System.out.print(n+" ");
         ArrayList<Integer> res = new ArrayList<>();
         int[] ans = new int[nums.length];
         HashMap<Integer,Integer> hm = new HashMap<>();
@@ -18,24 +21,38 @@ public class _234_Sort_Array_by_Increasing_Frequency {
             list.add(n);
         }
 
-        System.out.println(list);
 
         ArrayList<Integer> list1 = new ArrayList<>(list);
+        System.out.println(list1);
 
-        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
-            ArrayList<Integer> ls = new ArrayList<>();
+        while(res.size()!= nums.length){
             int c = 0;
-            if(entry.getValue() == list1.getFirst()){
-                ls.add(entry.getKey());
-                c++;
+            ArrayList<Integer> ls = new ArrayList<>();
+            for(Map.Entry<Integer,Integer> entry : hm.entrySet()) {
+
+                if(entry.getValue() == list1.getFirst()){
+                    ls.add(entry.getKey());
+                    c++;
+                }
             }
+            list1.removeFirst();
+            System.out.println("ls-->"+ls);
+            System.out.println("list1-->"+list1);
             if(c>1)
                 ls.sort((a,b)->b-a);
 
-            res.addAll(ls);
+            for(int num : ls){
+                for (int i = 0; i < hm.get(ls.getFirst()); i++) {
+                    res.add(num);
+                }
+            }
 
+            System.out.println("res-->"+res);
 
         }
+
         System.out.println(res);
+
+        Math.multiplyExact(2,2);
     }
 }
