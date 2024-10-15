67c44bdd319044a9274db4381f227f37810ff9d8
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2024-10-15 21:18:33 +0530

    DSA day 51 - bitwise question

---
+++ _235_Reverse_Bits.java
@@ -0,0 +1,8 @@
+package Bitwise_Operators;
+
+public class _235_Reverse_Bits {
+    public static void main(String[] args) {
+        int n = 43261596;
+        System.out.println(Integer.reverse(n));
+    }
+}
